193249364fb5e684bad1ee74cd191963b8048aef
Aykut Mayali <aykutmayali@gmail.com>
2024-01-09 17:40:28 +0300

    Unit Tests for ContactController

--- tests/ContactService.UnitTests/ContactControllerTests.cs
+++ tests/ContactService.UnitTests/ContactControllerTests.cs
@@ -2,8 +2,11 @@ using AutoFixture;
 using AutoMapper;
 using ContactService.Controllers;
 using ContactService.Data;
+using ContactService.DTOs;
+using ContactService.Entities;
 using ContactService.RequestHelpers;
 using MassTransit;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 namespace ContactService.UnitTests;
@@ -28,4 +31,45 @@ public class ContactControllerTests
         _mapper = new Mapper(mockMapper);
         _controller = new ContactsControllerForMock(_contactRepo.Object, _mapper, _publishEndpoint.Object);
     }
+
+    [Fact]
+    public async Task GetContacts_WithNoParams_Returns10Contatcs()
+    {
+        //arrange
+        var contacts = _fixture.CreateMany<ContactDto>(10).ToList();
+        _contactRepo.Setup(repo => repo.GetContactsAsync()).ReturnsAsync(contacts);
+        //act
+        var result = await _controller.GetAllContacts();
+        //assert
+        Assert.Equal(10, result.Value.Count);
+        Assert.IsType<ActionResult<List<ContactDto>>>(result);
+
+    }
+
+    [Fact]
+    public async Task GetContactById_WithValidGuid_ReturnsContact()
+    {
+        //arrange
+        var contact = _fixture.Create<ContactDto>();
+        _contactRepo.Setup(repo => repo.GetContactByIdAsync(It.IsAny<Guid>())).ReturnsAsync(contact);
+        //act
+        var result = await _controller.GetContactById(contact.Id);
+        //assert
+        Assert.Equal(contact.Name, result.Value.Name);
+        Assert.IsType<ActionResult<ContactDto>>(result);
+
+    }
+
+    [Fact]
+    public async Task GetContactById_WithInValidGuid_ReturnsNotFound()
+    {
+        //arrange
+        _contactRepo.Setup(repo => repo.GetContactByIdAsync(It.IsAny<Guid>())).ReturnsAsync(value:null);
+        //act
+        var result = await _controller.GetContactById(Guid.NewGuid());
+        //assert
+        Assert.IsType<NotFoundResult>(result.Result);
+
+    }
+
 }
