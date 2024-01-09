1e5681e1bd367a6536bd3fd6b6269f42d01a24b0
Aykut Mayali <aykutmayali@gmail.com>
2024-01-09 17:52:31 +0300

    Postman Collection Added

---
+++ README.md
@@ -0,0 +1,22 @@
+# PhoneBookReport Microservice Project
+
+## Contact Service
+- Postgres is used for Db
+
+## Report Service
+- MongoDb is used for Db
+
+## MessageQueue system 
+- RabbitMq
+
+## Testing
+- XUnit
+
+## Commands and Testing
+- Run : 
+    - docker compose up -d
+    for running dbs
+
+    - Postman Collection added for Api Testing
+    for using get reports filterBy :
+        use pageNumber (3) and filterBy (Location) as parameters
--- src/ContactService/Data/DbInitializer.cs
+++ src/ContactService/Data/DbInitializer.cs
@@ -32,26 +32,6 @@ public class DbInitializer
         new Contact { Id = Guid.NewGuid(), Name = "Jane", Surname = "Smith", 
         Company = new Company{
             Id = Guid.NewGuid(), CompanyName = "Company Jane", ContactType = ContactType.EmailAddress, DataContent ="jane@work.com"  
-        } },
-
-        new Contact { Id = Guid.NewGuid(), Name = "Bob", Surname = "Johnson", 
-        Company = new Company{
-            Id = Guid.NewGuid(), CompanyName = "Company Bob", ContactType = ContactType.EmailAddress, DataContent ="bob@work.com" 
-        } },
-
-        new Contact { Id = Guid.NewGuid(), Name = "Alice", Surname = "Williams", 
-        Company = new Company{
-            Id = Guid.NewGuid(), CompanyName = "Company Alice", ContactType = ContactType.MobileNumber, DataContent ="555 677 1121" 
-        } },
-
-        new Contact { Id = Guid.NewGuid(), Name = "Charlie", Surname = "Brown", 
-        Company = new Company{
-            Id = Guid.NewGuid(), CompanyName = "Company Charlie", ContactType = ContactType.Location, DataContent ="Canakkale" 
-        } },
-
-        new Contact { Id = Guid.NewGuid(), Name = "Jan", Surname = "Dark", 
-        Company = new Company{
-            Id = Guid.NewGuid(), CompanyName = "Company Dark", ContactType = ContactType.Location, DataContent ="Karaman"
         } }
     };
         //context.Contacts.AddRange(contacts);
